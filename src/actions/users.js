afc57b2092687edbd09819ac43f60056f76b1ef5
harsohailB <harsohail.brar1@gmail.com>
2020-07-21 15:23:10 -0600

    add user creds to local storage

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,10 +1,16 @@
 import { LOGIN_USER } from "./types";
 
 export const loginUser = (username, password) => {
-    // TODO use env file for storing username and password
-    if(username === "admin" && password === "password"){
-        return { type: LOGIN_USER, user: true }
-    }else{
-        throw "Login failed";
-    }
-}
+  // TODO use env file for storing username and password
+  if (username === "admin" && password === "password") {
+    return {
+      type: LOGIN_USER,
+      user: {
+        username: username,
+        password: password,
+      },
+    };
+  } else {
+    throw "Login failed";
+  }
+};
