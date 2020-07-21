0e0f8edf488c0205e2ac13e97bed2bce5f3d6a01
harsohailB <harsohail.brar1@gmail.com>
2020-07-21 14:40:55 -0600

    add put route to edit items and bind edit item button to it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect }from "react";
-import "./App.css"
-import Navbar from "./Navbar"
+import React, { useEffect } from "react";
+import "./App.css";
+import Navbar from "./Navbar";
 import AnnouncementHeader from "./AnnouncementHeader";
-import Footer from "./Footer"
-import HomePage from "./components/HomePage"
-import CatalogPage from "./components/CatalogPage"
-import StoryPage from "./components/StoryPage"
-import ContactPage from "./components/ContactPage"
+import Footer from "./Footer";
+import HomePage from "./components/HomePage";
+import CatalogPage from "./components/CatalogPage";
+import StoryPage from "./components/StoryPage";
+import ContactPage from "./components/ContactPage";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import ShoppingCartPage from "./components/ShoppingCartPage";
 import SearchPage from "./components/SearchPage";
@@ -16,6 +16,7 @@ import AddItemPage from "./components/AddItemPage";
 import ls from "local-storage";
 import { useDispatch } from "react-redux";
 import { LOGIN_USER, LOGOUT_USER, FETCH_SC_ITEMS } from "./actions/types";
+import EditItemPage from "./components/EditItemPage";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -31,30 +32,34 @@ const App = () => {
     }
 
     if (shoppingCartItemsFromLocalStorage) {
-      dispatch({ type: FETCH_SC_ITEMS, items: shoppingCartItemsFromLocalStorage });
+      dispatch({
+        type: FETCH_SC_ITEMS,
+        items: shoppingCartItemsFromLocalStorage,
+      });
     }
   }, [dispatch]);
 
-    return (
-        <BrowserRouter>
-              <div className="app-container">
-                <AnnouncementHeader/>
-                <Navbar/>
-                <Switch>
-                  <Route exact path="/" component={HomePage}></Route>
-                  <Route path="/catalog/:id" component={ItemPage}></Route>
-                  <Route path="/catalog" component={CatalogPage}></Route>
-                  <Route path="/story" component={StoryPage}></Route>
-                  <Route path="/contact" component={ContactPage}></Route>
-                  <Route path="/cart" component={ShoppingCartPage}></Route>
-                  <Route path="/search" component={SearchPage}></Route>
-                  <Route path="/login" component={LoginPage}></Route>
-                  <Route path="/add-item" component={AddItemPage}></Route>
-                </Switch>
-                <Footer/>
-              </div>
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        <AnnouncementHeader />
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/catalog/:id" component={ItemPage}></Route>
+          <Route path="/catalog" component={CatalogPage}></Route>
+          <Route path="/story" component={StoryPage}></Route>
+          <Route path="/contact" component={ContactPage}></Route>
+          <Route path="/cart" component={ShoppingCartPage}></Route>
+          <Route path="/search" component={SearchPage}></Route>
+          <Route path="/login" component={LoginPage}></Route>
+          <Route path="/add-item" component={AddItemPage}></Route>
+          <Route path="/edit-item/:id" component={EditItemPage}></Route>
+        </Switch>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/components/ItemPage/index.jsx
+++ src/components/ItemPage/index.jsx
@@ -2,231 +2,249 @@ import React, { useState, useEffect, useContext } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
-import Dropdown from "../styled/Dropdown"
-import Button from "../styled/Button"
+import Dropdown from "../styled/Dropdown";
+import Button from "../styled/Button";
 import { useLocation } from "react-router-dom";
 import { getItemById, getItems } from "../../actions/items";
 import { ADD_ITEM_TO_SC } from "../../actions/types";
 import Title from "../styled/Title";
 
 const Wrapper = styled.div`
-    display: flex;
-    align-items: flex-start;
-    margin-top: 50px;
+  display: flex;
+  align-items: flex-start;
+  margin-top: 50px;
 `;
 
 const PreviewWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-end;
-    max-width: 50%;
-    margin-right: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  max-width: 50%;
+  margin-right: 50px;
 `;
 
 const MainImageWrapper = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    overflow: hidden;
-    margin-bottom: 25px;
+  display: flex;
+  justify-content: flex-end;
+  overflow: hidden;
+  margin-bottom: 25px;
 `;
 
 const MainImage = styled.img`
-    width: 50%;
-    height: auto;
-    transition: all 0.3s ease;
+  width: 50%;
+  height: auto;
+  transition: all 0.3s ease;
 
-    &:hover {
-        transform: scale(3);
-    }
+  &:hover {
+    transform: scale(3);
+  }
 `;
 
 const SmallImageWrapper = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    flex-wrap: wrap;
-    flex-grow: 4;
-    width: 600px;
+  display: flex;
+  justify-content: flex-end;
+  flex-wrap: wrap;
+  flex-grow: 4;
+  width: 600px;
 `;
 
 const SmallImage = styled.img`
-    width: 110px;
-    height: auto;
-    cursor: pointer;
-    margin: 10px;
-    padding: 2px;
+  width: 110px;
+  height: auto;
+  cursor: pointer;
+  margin: 10px;
+  padding: 2px;
 
-    &:hover {
-        opacity: 80%;
-    }
+  &:hover {
+    opacity: 80%;
+  }
 `;
 
 const InfoWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    width: 20%;
-    margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 20%;
+  margin-top: 50px;
 `;
 
 const Name = styled.h3`
-    font-family: Righteous,sans-serif;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 36px;
-    font-weight: 400;
-    color: #3d4246;
-    line-height: 1.5;
-    margin: 0;
+  font-family: Righteous, sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  font-weight: 400;
+  color: #3d4246;
+  line-height: 1.5;
+  margin: 0;
 `;
 
 const Price = styled.h3`
-    font-family: Oswald,sans-serif;
-    font-style: normal;
-    font-size: 18px;
-    font-weight: 200;
-    color: #727A83;
-    margin: 0;
+  font-family: Oswald, sans-serif;
+  font-style: normal;
+  font-size: 18px;
+  font-weight: 200;
+  color: #727a83;
+  margin: 0;
 `;
 
 const Description = styled.h3`
-    font-family: Oswald,sans-serif;
-    font-style: normal;
-    font-size: 16px;
-    font-weight: 200;
-    color: #727A83;
-    margin-top: 25px;
+  font-family: Oswald, sans-serif;
+  font-style: normal;
+  font-size: 16px;
+  font-weight: 200;
+  color: #727a83;
+  margin-top: 25px;
 `;
 
 const DropdownWrapper = styled.div`
-    display: flex;
-    flex-grow: 2;
-    margin-top: 25px;
-    margin-bottom: 5px;
+  display: flex;
+  flex-grow: 2;
+  margin-top: 25px;
+  margin-bottom: 5px;
 `;
 
 const ItemPage = () => {
-    const user = useSelector((state) => state.user);
-    const shoppingCartItems = useSelector((state) => state.shoppingCart);
-    const dispatch = useDispatch();
-    const location = useLocation();
-    const history = useHistory();
-    const [item, setItem] = useState(null);
-    const [itemColors, setItemColors] = useState(null);
-    const [itemSizes, setItemSizes] = useState(null);
-    const [images, setImages] = useState([]);
-    const [mainImage, setMainImage] = useState(null);
-    const [selectedColor, setSelectedColor] = useState(null);
-    const [selectedSize, setSelectedSize] = useState(null);
+  const user = useSelector((state) => state.user);
+  const shoppingCartItems = useSelector((state) => state.shoppingCart);
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const history = useHistory();
+  const [item, setItem] = useState(null);
+  const [itemColors, setItemColors] = useState(null);
+  const [itemSizes, setItemSizes] = useState(null);
+  const [images, setImages] = useState([]);
+  const [mainImage, setMainImage] = useState(null);
+  const [selectedColor, setSelectedColor] = useState(null);
+  const [selectedSize, setSelectedSize] = useState(null);
 
-    useEffect(() => {
-        try {
-            const itemId = location.pathname.split("/").reverse()[0];
-            getItemById(itemId).then(fetchedItem => {
-                setItem(fetchedItem);
-                setImages(fetchedItem.images);
-                setItemColors(fetchedItem.colors.split('/'));
-                setSelectedColor(fetchedItem.colors.split('/')[0]);
-                setItemSizes(fetchedItem.sizes.split('/'));
-                setSelectedSize(fetchedItem.sizes.split('/')[0]);
-                setMainImage(fetchedItem.thumbnailImage);
-            })
-        } catch (e) {
-            console.log("ItemPage UseEffect ERROR")
-            console.log(e);
-        }
-    }, [location]);
-
-    const renderItemColors = () => {
-        const itemColors = item.colors.split('/')
-        return itemColors.map(color => (
-            <option>{color}</option>
-        ));
-    };
-
-    const renderItemSizes = () => {
-        const itemSizes = item.sizes.split('/')
-        return itemSizes.map(size => (
-            <option>{size}</option>
-        ));
-    };
-
-    const renderSmallImages = () => {
-        return images.map(image => (
-            <SmallImage 
-                src={require("../../assets/catalog/inventory/" + item.folderName + "/" + image.imageName)}
-                onClick={() => setMainImage(image)}
-            ></SmallImage>
-        ));
+  useEffect(() => {
+    try {
+      const itemId = location.pathname.split("/").reverse()[0];
+      getItemById(itemId).then((fetchedItem) => {
+        setItem(fetchedItem);
+        setImages(fetchedItem.images);
+        setItemColors(fetchedItem.colors.split("/"));
+        setSelectedColor(fetchedItem.colors.split("/")[0]);
+        setItemSizes(fetchedItem.sizes.split("/"));
+        setSelectedSize(fetchedItem.sizes.split("/")[0]);
+        setMainImage(fetchedItem.thumbnailImage);
+      });
+    } catch (e) {
+      console.log("ItemPage UseEffect ERROR");
+      console.log(e);
     }
+  }, [location]);
 
-    const updateSelectedColor = evt => {
-        setSelectedColor(evt.target.value);
-        images.forEach(image => {
-            if(image.color === evt.target.value){
-                setMainImage(image);
-            }
-        });
-    }
-    
-    const updateSelectedSize = evt => {
-        setSelectedSize(evt.target.value);
-    }
-    
-    const handleRemoveItem = () => {
-        // TODO remove item
-    }
-    
-    const handleEditItem = () => {
-        // TODO edit item
-    }
+  const renderItemColors = () => {
+    const itemColors = item.colors.split("/");
+    return itemColors.map((color) => <option>{color}</option>);
+  };
 
-    const handleAddToCartClick = () => {
-        console.log(item);
-        dispatch({ type: ADD_ITEM_TO_SC, 
-                   item: {
-                    ...item,
-                    selectedImageName: mainImage.imageName,
-                    color: selectedColor,
-                    quantity: "1",
-                    size: selectedSize
-                   }
-                });
-        history.push("/cart");
-    }
+  const renderItemSizes = () => {
+    const itemSizes = item.sizes.split("/");
+    return itemSizes.map((size) => <option>{size}</option>);
+  };
 
-    return(
-        <div>
-            {item ? 
-                <Wrapper>
-                    <PreviewWrapper>
-                        <MainImageWrapper>
-                            {mainImage ? 
-                                <MainImage src={require("../../assets/catalog/inventory/" + item.folderName + "/" + mainImage.imageName)}></MainImage>
-                            :   <MainImage src={require("../../assets/catalog/inventory/" + item.folderName + "/" + item.thumbnailImage.imageName)}></MainImage>}
-                        </MainImageWrapper>
+  const renderSmallImages = () => {
+    return images.map((image) => (
+      <SmallImage
+        src={require("../../assets/catalog/inventory/" +
+          item.folderName +
+          "/" +
+          image.imageName)}
+        onClick={() => setMainImage(image)}
+      ></SmallImage>
+    ));
+  };
 
-                        <SmallImageWrapper>
-                            {renderSmallImages()}
-                        </SmallImageWrapper>
-                    </PreviewWrapper>
+  const updateSelectedColor = (evt) => {
+    setSelectedColor(evt.target.value);
+    images.forEach((image) => {
+      if (image.color === evt.target.value) {
+        setMainImage(image);
+      }
+    });
+  };
 
-                    <InfoWrapper>
-                        <Name>{item.name}</Name>
-                        <Price>${item.price}</Price>
-                        <DropdownWrapper>
-                            <Dropdown value={selectedColor} onChange={updateSelectedColor}>{renderItemColors()}</Dropdown>
-                            <Dropdown value={selectedSize} onChange={updateSelectedSize}>{renderItemSizes()}</Dropdown>
-                        </DropdownWrapper>
-                        <Button onClick={handleAddToCartClick} to="/cart">ADD TO CART</Button>
-                        <Description>{item.description}</Description>
-                        {user && <Button onClick={handleEditItem}>EDIT ITEM</Button>}
-                        {user && <Button onClick={handleRemoveItem}>REMOVE ITEM</Button>}
-                    </InfoWrapper>
-                </Wrapper> : 
-                <Title>Loading...</Title>}
-        </div>
-    );
-}
+  const updateSelectedSize = (evt) => {
+    setSelectedSize(evt.target.value);
+  };
 
-export default ItemPage
+  const handleRemoveItem = () => {
+    // TODO remove item
+  };
+
+  const handleEditItem = () => {
+    history.push("/edit-item/" + item.id);
+  };
+
+  const handleAddToCartClick = () => {
+    console.log(item);
+    dispatch({
+      type: ADD_ITEM_TO_SC,
+      item: {
+        ...item,
+        selectedImageName: mainImage.imageName,
+        color: selectedColor,
+        quantity: "1",
+        size: selectedSize,
+      },
+    });
+    history.push("/cart");
+  };
+
+  return (
+    <div>
+      {item ? (
+        <Wrapper>
+          <PreviewWrapper>
+            <MainImageWrapper>
+              {mainImage ? (
+                <MainImage
+                  src={require("../../assets/catalog/inventory/" +
+                    item.folderName +
+                    "/" +
+                    mainImage.imageName)}
+                ></MainImage>
+              ) : (
+                <MainImage
+                  src={require("../../assets/catalog/inventory/" +
+                    item.folderName +
+                    "/" +
+                    item.thumbnailImage.imageName)}
+                ></MainImage>
+              )}
+            </MainImageWrapper>
+
+            <SmallImageWrapper>{renderSmallImages()}</SmallImageWrapper>
+          </PreviewWrapper>
+
+          <InfoWrapper>
+            <Name>{item.name}</Name>
+            <Price>${item.price}</Price>
+            <DropdownWrapper>
+              <Dropdown value={selectedColor} onChange={updateSelectedColor}>
+                {renderItemColors()}
+              </Dropdown>
+              <Dropdown value={selectedSize} onChange={updateSelectedSize}>
+                {renderItemSizes()}
+              </Dropdown>
+            </DropdownWrapper>
+            <Button onClick={handleAddToCartClick} to="/cart">
+              ADD TO CART
+            </Button>
+            <Description>{item.description}</Description>
+            {user && <Button onClick={handleEditItem}>EDIT ITEM</Button>}
+            {user && <Button onClick={handleRemoveItem}>REMOVE ITEM</Button>}
+          </InfoWrapper>
+        </Wrapper>
+      ) : (
+        <Title>Loading...</Title>
+      )}
+    </div>
+  );
+};
+
+export default ItemPage;
