a49996e355ebbc46bfd76cd7a7078c8f23d58255
harsohailB <harsohail.brar1@gmail.com>
2020-07-21 18:34:02 -0600

    redirect to stripe checkout

--- server.js
+++ server.js
@@ -1,36 +1,66 @@
 // Server setup
-const jsonServer = require('json-server');
-const { readSync } = require('fs');
-const { RSA_NO_PADDING } = require('constants');
+const jsonServer = require("json-server");
+const { readSync } = require("fs");
+const { RSA_NO_PADDING } = require("constants");
 const server = jsonServer.create();
-const dbPath = './src/assets/catalog/inventoryData.json';
+const dbPath = "./src/assets/catalog/inventoryData.json";
 const router = jsonServer.router(dbPath);
 const middlewares = jsonServer.defaults();
- 
+require("dotenv").config();
+
 // Set default middlewares
 server.use(middlewares);
 
-const isAuthorizableRequest = req => {
-  return req.method === 'POST' || req.method === 'PUT' || req.method === 'DELETE' || req.method === 'PATCH';
-}
+const isAuthorizableRequest = (req) => {
+  if (req.originalUrl === "/session_id") {
+    return false;
+  } else {
+    return (
+      req.method === "POST" ||
+      req.method === "PUT" ||
+      req.method === "DELETE" ||
+      req.method === "PATCH"
+    );
+  }
+};
 
-const isAuthorized = req => {
+const isAuthorized = (req) => {
   // TODO use env for store username and password
-  return (req.query.username === "admin" && req.query.password === "password");
-}
+  return (
+    req.query.username === process.env.ADMIN_USERNAME &&
+    req.query.password === process.env.ADMIN_PASSWORD
+  );
+};
 
 // Server POST, PUT, DELETE, PATCH auth
 server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-  if(isAuthorizableRequest(req) && !isAuthorized(req)){
+  if (isAuthorizableRequest(req) && !isAuthorized(req)) {
     res.sendStatus(401);
-  }else{
+  } else {
     next();
   }
-})
+});
 
-// Use default router 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+// Stripe checkout session route
+server.post("/session_id", async (req, res) => {
+  let session = await stripe.checkout.sessions
+    .create({
+      payment_method_types: ["card"],
+      line_items: req.body.line_items,
+      success_url: "http://localhost:3000/",
+      cancel_url: "http://localhost:3000/cart",
+    })
+    .catch((err) => {
+      throw err;
+    });
+  res.jsonp(session.id);
+});
+
+// Use default router
 server.use(router);
 server.listen(3001, () => {
-  console.log('JAMBO back-end server is running')
+  console.log("JAMBO back-end server is running");
 });
