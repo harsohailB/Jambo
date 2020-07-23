a0f4c78ad9e8f2c6579ebaee75c2065b78af1345
Harsohail Brar <47438886+harsohailB@users.noreply.github.com>
2020-07-22 19:21:32 -0600

    Feature/stripe payments (#23)
    * redirect to stripe checkout
    
    * add shipping address

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect }from "react";
-import "./App.css"
-import Navbar from "./Navbar"
+import React, { useEffect } from "react";
+import "./App.css";
+import Navbar from "./Navbar";
 import AnnouncementHeader from "./AnnouncementHeader";
-import Footer from "./Footer"
-import HomePage from "./components/HomePage"
-import CatalogPage from "./components/CatalogPage"
-import StoryPage from "./components/StoryPage"
-import ContactPage from "./components/ContactPage"
+import Footer from "./Footer";
+import HomePage from "./components/HomePage";
+import CatalogPage from "./components/CatalogPage";
+import StoryPage from "./components/StoryPage";
+import ContactPage from "./components/ContactPage";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import ShoppingCartPage from "./components/ShoppingCartPage";
 import SearchPage from "./components/SearchPage";
@@ -31,30 +31,33 @@ const App = () => {
     }
 
     if (shoppingCartItemsFromLocalStorage) {
-      dispatch({ type: FETCH_SC_ITEMS, items: shoppingCartItemsFromLocalStorage });
+      dispatch({
+        type: FETCH_SC_ITEMS,
+        items: shoppingCartItemsFromLocalStorage,
+      });
     }
   }, [dispatch]);
 
-    return (
-        <BrowserRouter>
-              <div className="app-container">
-                <AnnouncementHeader/>
-                <Navbar/>
-                <Switch>
-                  <Route exact path="/" component={HomePage}></Route>
-                  <Route path="/catalog/:id" component={ItemPage}></Route>
-                  <Route path="/catalog" component={CatalogPage}></Route>
-                  <Route path="/story" component={StoryPage}></Route>
-                  <Route path="/contact" component={ContactPage}></Route>
-                  <Route path="/cart" component={ShoppingCartPage}></Route>
-                  <Route path="/search" component={SearchPage}></Route>
-                  <Route path="/login" component={LoginPage}></Route>
-                  <Route path="/add-item" component={AddItemPage}></Route>
-                </Switch>
-                <Footer/>
-              </div>
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        <AnnouncementHeader />
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/catalog/:id" component={ItemPage}></Route>
+          <Route path="/catalog" component={CatalogPage}></Route>
+          <Route path="/story" component={StoryPage}></Route>
+          <Route path="/contact" component={ContactPage}></Route>
+          <Route path="/cart" component={ShoppingCartPage}></Route>
+          <Route path="/search" component={SearchPage}></Route>
+          <Route path="/login" component={LoginPage}></Route>
+          <Route path="/add-item" component={AddItemPage}></Route>
+        </Switch>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
