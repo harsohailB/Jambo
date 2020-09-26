fd561a0d11b1e2496b55e44c72575fb2edf270a4
harsohailB <harsohail.brar1@ucalgary.ca>
2020-09-26 12:16:16 -0600

    delete redirect to homepage and change succes message

--- src/components/SuccessPage/index.jsx
+++ src/components/SuccessPage/index.jsx
@@ -13,27 +13,40 @@ const Wrapper = styled.div`
   justify-content: center;
 `;
 
+const Subtitle = styled.h1`
+  font-size: 16px;
+  font-family: Oswald, sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  color: #838b92;
+  line-height: 1.5;
+  max-width: 50%;
+  margin-bottom: 25px;
+  text-align: center;
+`;
+
 const SuccessPage = () => {
   const shoppingCartItems = useSelector((state) => state.shoppingCart);
   const dispatch = useDispatch();
   const history = useHistory();
 
   useEffect(() => {
-    setTimeout(() => {
-      dispatch({
-        type: CLEAR_CART,
-      });
-      history.push("/");
-    }, 1000);
+    dispatch({
+      type: CLEAR_CART,
+    });
   }, []);
 
   return (
     <Wrapper>
       <Helmet>
-        <title>Checkout Success - JAMBO</title>
+        <title>Order Success - JAMBO</title>
       </Helmet>
-      <Title>Checkout Success!</Title>
-      <Title>Redirecting...</Title>
+      <Title>Order Success!</Title>
+      <Subtitle>
+        You will receive an email confirmation for your order shortly, if you
+        have any questions about your feel free to contact us at{" "}
+        <a href="jamboapparel@gmail.com">jamboapparel@gmail.com</a>
+      </Subtitle>
     </Wrapper>
   );
 };
