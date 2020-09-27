326629e4091356db0899968452f2b6b8d6a0f97f
harsohailB <harsohail.brar1@ucalgary.ca>
2020-09-27 15:00:31 -0600

    change api url to production

--- src/config.js
+++ src/config.js
@@ -1,26 +1,26 @@
 // TODO comment in the production code and comment out the development code when deployed
 // Production
-// let config = {
-//   api: {
-//     protocol: "https",
-//     host: "localhost:3001",
-//   },
-// };
-
-// config.endpoint = config.api.host;
-
-// module.exports = config;
-
-// Local Developement
 let config = {
   api: {
-    protocol: "http",
-    host: "localhost",
-    port: 3001,
+    protocol: "https",
+    host: "jambo-backend.herokuapp.com",
   },
 };
 
-config.endpoint =
-  config.api.protocol + "://" + config.api.host + ":" + config.api.port;
+config.endpoint = config.api.protocol + "://" + config.api.host;
 
 module.exports = config;
+
+// Local Developement
+// let config = {
+//   api: {
+//     protocol: "http",
+//     host: "localhost",
+//     port: 3001,
+//   },
+// };
+
+// config.endpoint =
+//   config.api.protocol + "://" + config.api.host + ":" + config.api.port;
+
+// module.exports = config;
