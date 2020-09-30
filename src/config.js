2994e2a41fc7efbf2564e07018432d9c89b67e67
harsohailB <harsohail.brar1@ucalgary.ca>
2020-09-29 21:00:48 -0600

    add developemnt config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+// TODO comment in the production code and comment out the development code when deployed
+// Production
 let config = {
   api: {
     protocol: "https",
@@ -8,3 +10,17 @@ let config = {
 config.endpoint = config.api.protocol + "://" + config.api.host;
 
 module.exports = config;
+
+// Local Developement
+// let config = {
+//   api: {
+//     protocol: "http",
+//     host: "localhost",
+//     port: 3001,
+//   },
+// };
+
+// config.endpoint =
+//   config.api.protocol + "://" + config.api.host + ":" + config.api.port;
+
+// module.exports = config;
