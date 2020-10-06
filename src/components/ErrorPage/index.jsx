9a4b6168c78ecd4bff085dd1bd71839144bf3218
harsohailB <harsohail.brar1@ucalgary.ca>
2020-10-05 18:11:45 -0600

    add svg on error page

--- src/components/ErrorPage/index.jsx
+++ src/components/ErrorPage/index.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 import Title from "../styled/Title";
+import errorSVG from "../../assets/svgs/404.svg";
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Image = styled.img`
+  width: 30vw;
+`;
 
 const ErrorPage = () => {
-  return <Title>This page does not exist... (Error 404)</Title>;
+  return (
+    <Wrapper>
+      <Title>This page does not exist...</Title>
+      <Image src={errorSVG}></Image>
+    </Wrapper>
+  );
 };
 
 export default ErrorPage;
