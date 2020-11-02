07c640f90edc55c587192c87c912991fb0b82d7d
harsohailB <harsohail.brar1@ucalgary.ca>
2020-11-01 18:28:17 -0700

    update backend config

--- src/config.js
+++ src/config.js
@@ -1,24 +1,24 @@
 // TODO comment in the production code and comment out the development code when deployed
 // Production
-// let config = {
-//   api: {
-//     protocol: "https",
-//     host: "jambo-backend.herokuapp.com",
-//   },
-// };
-
-// config.endpoint = config.api.protocol + "://" + config.api.host;
-
-// module.exports = config;
-
-// Local Development
 let config = {
   api: {
-    protocol: "http",
-    host: "localhost",
-    port: 9000,
+    protocol: "https",
+    host: "jambo-express-backend.herokuapp.com",
   },
 };
+
+config.endpoint = config.api.protocol + "://" + config.api.host;
+
+module.exports = config;
+
+// Local Development
+// let config = {
+//   api: {
+//     protocol: "http",
+//     host: "localhost",
+//     port: 9000,
+//   },
+// };
 
 config.endpoint =
   config.api.protocol + "://" + config.api.host + ":" + config.api.port;
