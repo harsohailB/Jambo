e912d493f3be17413a3774feefd2a3bc53714499
unknown <gary.wu2@ucalgary.ca>
2020-11-11 18:21:48 -0700

    Added Social Media Links to Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,50 +1,57 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { SocialIcon } from 'react-social-icons';
 
-import paymentMethods from "./assets/updated-payment-methods.png";
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
-  margin-top: 50px;
-
+  margin-top: 38px;
+  margin-bottom: 15px;
   @media (max-width: 900px) {
     margin-top: 100px;
   }
 `;
 
-const PageReference = styled(Link)`
-  font-size: 16px;
-  font-family: Oswald, sans-serif;
-  font-style: normal;
-  font-weight: 400;
-  color: #3d4246;
-  line-height: 1.5;
-  text-decoration: none;
-  margin: 20px;
-`;
-
-const Image = styled.img`
-  max-height: 32px;
-`;
-
 const Credits = styled.p`
-  font-size: 12px;
+  font-size: 14px;
   font-family: Oswald, sans-serif;
   font-style: normal;
   font-weight: 400;
   color: #3d4246;
   line-height: 1.5;
+`;
+
+const SocialMediaIcons = styled.div`
+  display: flex;
+  flex-flow: row ;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  font-family: Righteous, sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.2;
+  font-size: 1.25em;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+  text-align: center;
+  color: #3d4246;
 `;
 
 const Footer = () => {
   return (
     <Wrapper>
-      <Image src={paymentMethods}></Image>
+      <Title>Check us out and follow us on Facebook and Instagram</Title>
+      <SocialMediaIcons>
+        <SocialIcon url="https://www.facebook.com/jamboapparell" style={{ height: 50, width: 50, margin: 10}}/>
+        <SocialIcon url="https://www.instagram.com/jamboapparel/?igshid=cduznovgcg5p" style={{ height: 50, width: 50, margin: 10}}/>
+      </SocialMediaIcons>
       <Credits>
-        © 2020, JAMBO Website by Harsohail Brar & Ryan Holt & Gary Wu
+        © 2020, JAMBO Website by Harsohail Brar, Ryan Holt & Gary Wu
       </Credits>
     </Wrapper>
   );
