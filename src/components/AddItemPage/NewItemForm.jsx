91239bdc5402da93633d8abae0cc50778cd5b652
harsohailB <harsohail.brar1@ucalgary.ca>
2020-11-15 14:02:07 -0700

    ensure all colors are assigned, if not they are removed from dropdown

--- src/components/AddItemPage/NewItemForm.jsx
+++ src/components/AddItemPage/NewItemForm.jsx
@@ -104,6 +104,14 @@ const Label = styled.label`
   margin-left: 10px;
 `;
 
+const List = styled.li`
+  font-size: 16px;
+  font-family: Oswald, sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  color: black;
+`;
+
 const Icon = styled.div`
   margin: 5px;
   color: #557b97;
@@ -151,7 +159,7 @@ const NewItemForm = (props) => {
       getItemById(props.item.id)
         .then((fetchedItem) => {
           console.log(props.item);
-          setNewItem(fetchedItem);
+          setNewItem(pruneItemColors(fetchedItem));
           setHasErrors(false);
         })
         .catch((err) => {
@@ -160,6 +168,17 @@ const NewItemForm = (props) => {
         });
     }
   }, []);
+
+  const pruneItemColors = (item) => {
+    return {
+      ...item,
+      colors: item.images
+        .filter((image) => {
+          return item.colors.includes(image.color);
+        })
+        .map((image) => image.color),
+    };
+  };
 
   const getArrayOfColours = () => {
     const arrOfColours = newItem.colors;
@@ -201,7 +220,7 @@ const NewItemForm = (props) => {
       thumbnailImage: newItem.images[0],
       tags: newItem.tags,
     };
-    console.log("newItem", tempNewItem);
+    tempNewItem = pruneItemColors(newItem);
     if (!checkForErrors()) {
       try {
         if (props.edit) {
@@ -331,6 +350,12 @@ const NewItemForm = (props) => {
       increment: evt.target.value,
     });
   };
+
+  const getUnchosenColors = (chosenColors) => {
+    return newItem.colors.filter((color) => !chosenColors.includes(color));
+  };
+
+  const unchosenColors = getUnchosenColors(pruneItemColors(newItem).colors);
 
   return (
     <Wrapper>
@@ -477,6 +502,21 @@ const NewItemForm = (props) => {
             setNewItem={setNewItem}
           />
           {hasErrors && <Error>Please enter valid details!</Error>}
+
+          {unchosenColors.length !== 0 && (
+            <div>
+              <Label style={{ width: "100%" }}>
+                <strong>The following colors have not been assigned:</strong>{" "}
+              </Label>
+              {unchosenColors.map((color) => (
+                <List>{color}</List>
+              ))}
+              <Label style={{ width: "100%", color: "red" }}>
+                Upon creating the item, these will be removed if not assigned
+              </Label>
+            </div>
+          )}
+
           <Button>{props.edit ? "UPDATE ITEM" : "CREATE ITEM"}</Button>
         </Form>
       </FormWrapper>
